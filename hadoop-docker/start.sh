c2d6d1e2f1037860e4a9777f4489cd80d5b54ee2
pranav <pranav@pranav.lan>
2020-02-11 11:27:42 +0100

    added docker file need just to recreate docker

--- hadoop-docker/start.sh
+++ hadoop-docker/start.sh
@@ -9,7 +9,7 @@ start-yarn.sh
 hdfs dfs -mkdir "$HADOOP_DATA" && \
    hdfs dfs -mkdir "$HADOOP_DATA/original_data" && \
    hdfs dfs -moveFromLocal \
-     "Project/data/kindle_store.json" \
+     "/Project/data/kindle_store.json" \
      "$HADOOP_DATA/original_data/kindlestore.json"
 
 bash
